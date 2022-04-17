47b50e722eb3e53dfc06baf416ae775c5f414011
unknown <isht.asem@gmail.com>
2022-04-17 10:48:44 +0300

    host changes

--- src/lambdaExpression/Main.java
+++ src/lambdaExpression/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Introduction:");
 		Printable printable = () -> {
 			System.out.println("Hi there");
 		};
